e1dae3550a038fc0954988d2618fb72c979dbf14
Ahamed Hamas <hamasafa.21@uom.lk>
2026-02-16 15:15:47 +0530

    Hide ConsultationHeader Buttons in CreateConsultation page

--- src/app/consultations/ConsultationsContainer.tsx
+++ src/app/consultations/ConsultationsContainer.tsx
@@ -87,35 +87,39 @@ export const ConsultationsContainer = () => {
             {t("consultation.table.title")}
           </div>
           <div className="flex gap-2 flex-col-reverse md:flex-row">
-            <button
-              onClick={() => openAndCloseFilter()}
-              className={clsx(
-                "flex gap-2 cursor-pointer items-center justify-center md:justify-start border-2 rounded-lg px-4 py-2 text-sm",
-                openFilter
-                  ? "text-white bg-secondary border-secondary"
-                  : "text-secondary  border-secondary-100"
-              )}
-            >
-              <img
-                className={clsx("w-4", openFilter ? "invert-100" : "")}
-                src="https://img.icons8.com/ios/50/horizontal-settings-mixer--v1.png"
-                alt="horizontal-settings-mixer--v1"
-              />
-              {t("global.text.filter")}
-            </button>
-            <button
-              onClick={() => setIsCreatingConsultation(true)}
-              className="flex gap-2 cursor-pointer items-center justify-center md:justify-start rounded-lg px-4 py-2 bg-primary-600 text-white text-sm"
-            >
-              <img
-                className="w-4 invert"
-                src="https://img.icons8.com/ios-glyphs/30/plus-math.png"
-                alt="plus-math"
-              />
-              {t("consultation.create_consultation.button", {
-                type: "Consultation",
-              })}
-            </button>
+            {!isCreatingConsultation && (
+              <button
+                onClick={() => openAndCloseFilter()}
+                className={clsx(
+                  "flex gap-2 cursor-pointer items-center justify-center md:justify-start border-2 rounded-lg px-4 py-2 text-sm",
+                  openFilter
+                    ? "text-white bg-secondary border-secondary"
+                    : "text-secondary  border-secondary-100"
+                )}
+              >
+                <img
+                  className={clsx("w-4", openFilter ? "invert-100" : "")}
+                  src="https://img.icons8.com/ios/50/horizontal-settings-mixer--v1.png"
+                  alt="horizontal-settings-mixer--v1"
+                />
+                {t("global.text.filter")}
+              </button>
+            )}
+            {!isCreatingConsultation && (
+              <button
+                onClick={() => setIsCreatingConsultation(true)}
+                className="flex gap-2 cursor-pointer items-center justify-center md:justify-start rounded-lg px-4 py-2 bg-primary-600 text-white text-sm"
+              >
+                <img
+                  className="w-4 invert"
+                  src="https://img.icons8.com/ios-glyphs/30/plus-math.png"
+                  alt="plus-math"
+                />
+                {t("consultation.create_consultation.button", {
+                  type: "Consultation",
+                })}
+              </button>
+            )}
           </div>
         </div>
         <ConsultationsFilters
